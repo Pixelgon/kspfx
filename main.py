499b31075d629ba68e7e1eb642091404e3dac91a
pixelgon <pixelgon4@gmail.com>
2022-04-26 22:57:00 +0200

    It works!

--- main.py
+++ main.py
@@ -1,47 +1,38 @@
 from baseconvert.baseconvert import base
 
-
 origin = open("01.in", "r")
 output = open("01.out", "w")
 
-def split(tupi):
-    return [char for char in tupi]
-
 num = tuple(map(int, origin.readline().strip()))
-numin = base(num, 10, 2)
-zerin = 0
-for x in numin:
-    if x == 0:
-        zerin = zerin + 1
-    else:
-        zerin = 0
-numout = ''
-numlen = zerin + 1
-numbase = 2
-zer = 0
-baseout = 2
-
-while zerin < numlen:
-    numbase = numbase + 1
-    numout = base(numin, 2, numbase,)
-    numlen = len(numout)
-    for i in numout:
+zaklad_soustavy = 2
+zaklad_soustavy_max = 0
+pocet_nul_max = 0
+while True:
+    copynum = num
+    kolekce = base(num, 10, zaklad_soustavy)
+    pocet_nul = 0
+    zaklad_soustavy = zaklad_soustavy + 1
+    print(kolekce)
+    print(zaklad_soustavy)
+    for i in kolekce:
         if i == 0:
-            zer = zer + 1
+            pocet_nul = pocet_nul + 1
+        else:
+            pocet_nul = 0
         if len(str(i)) >= 2:
             tupi = [int(z) for z in str(i)]
             for y in tupi:
                 if y == 0:
-                    zer = zer + 1
+                    pocet_nul = 0 + 1
                 else:
-                    zer = 0
-        else:
-            zer = 0
-    if zer > zerin:
-        zerin = zer
-        baseout = numbase
-
-output.write(str(baseout))
+                    pocet_nul = 0
+    print(pocet_nul)
+    if pocet_nul >= pocet_nul_max:
+        pocet_nul_max = pocet_nul
+        zaklad_soustavy_max = zaklad_soustavy
+    if len(kolekce) <= pocet_nul_max:
+        output.write(str(zaklad_soustavy_max - 1))
+        break
 
 origin.close()
 output.close()
